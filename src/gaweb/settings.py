ebc9ffb369b309dda03a48790f8ee1bb833e0327
iamthemca <cihadaydin@std.sehir.edu.tr>
2020-07-20 13:51:15 +0100

    Postgres DB

--- src/gaweb/settings.py
+++ src/gaweb/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'u&z_kp9^mq=mji133v57ks(a5huh17^^!ozii(9rg+onxywbdk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
+DEBUG = True
 
-DEBUG = int(os.environ.get('DEBUG',default=1))
+#DEBUG = int(os.environ.get('DEBUG',default=1))
 
 ALLOWED_HOSTS = ['178.62.116.125']
 
@@ -65,13 +65,25 @@ WSGI_APPLICATION = 'gaweb.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if Debug:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'galtd',
+            'USER': 'mca',
+            'PASSWORD': 'testing123.',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
 
 
 # Password validation
