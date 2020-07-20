f4f0c1a49be95090ae4db5d2873cf7c5faf6b62f
iamthemca <cihadaydin@std.sehir.edu.tr>
2020-07-20 16:23:12 +0100

    static

--- src/gaweb/settings.py
+++ src/gaweb/settings.py
@@ -125,5 +125,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL='/media/'
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static_in_env')]
-STATIC_ROOT=os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
