83752d47b1fa8badd9253b16d96b57449b035572
iamthemca <cihadaydin@std.sehir.edu.tr>
2020-07-20 12:58:51 +0100

    added production dependencies

--- src/gaweb/settings.py
+++ src/gaweb/settings.py
@@ -12,7 +12,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'u&z_kp9^mq=mji133v57ks(a5huh17^^!ozii(9rg+onxywbdk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+
+DEBUG = int(os.environ.get('DEBUG',default=1))
 
 ALLOWED_HOSTS = []
 
