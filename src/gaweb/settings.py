d0e6fb352fee63edb1d889e02d019106710af872
iamthemca <cihadaydin@std.sehir.edu.tr>
2020-07-20 13:54:49 +0100

    Debug change

--- src/gaweb/settings.py
+++ src/gaweb/settings.py
@@ -65,7 +65,7 @@ WSGI_APPLICATION = 'gaweb.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-if Debug:
+if DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
